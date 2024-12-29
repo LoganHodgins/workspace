6efe6b5ec309bdf98eab7dfe9a0a99a893533f45
Logan Hodgins <loganhodginsofficial@gmail.com>
2024-12-28 23:35:34 -0600

    CS-1111 stuff

--- express-tutorial/src/index.mjs
+++ express-tutorial/src/index.mjs
@@ -1,4 +1,11 @@
-import express from 'express';
+import express from "express";
+import {
+  query,
+  validationResult,
+  matchedData,
+  checkSchema,
+} from "express-validator";
+import { createUserValidationSchema } from "./utils/validationSchemas.mjs";
 
 const app = express();
 
@@ -7,86 +14,110 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 const mockUsers = [
-    {id: 1, username: "logan", displayName: "Logan"},
-    {id: 2, username: "alice", displayName: "Alice"},
-    {id: 3, username: "bob", displayName: "Bob"}
-]
+  { id: 1, username: "logan", displayName: "Logan" },
+  { id: 2, username: "alice", displayName: "Alice" },
+  { id: 3, username: "bob", displayName: "Bob" },
+];
 
 const loggingMiddleware = (request, response, next) => {
-    console.log(`${request.method} - ${request.url}`);
-    next();
+  console.log(`${request.method} - ${request.url}`);
+  next();
 };
 
 const resolveIndexByUserId = (request, response, next) => {
-    const {
-        params: { id }
-    } = request;
-    const parsedId = parseInt(id);
-    if (isNaN(parsedId)) return response.sendStatus(400);
-    const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
-    if (findUserIndex === -1) return response.sendStatus(404);
-    request.findUserIndex = findUserIndex;
-    next();
+  const {
+    params: { id },
+  } = request;
+  const parsedId = parseInt(id);
+  if (isNaN(parsedId)) return response.sendStatus(400);
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
+  if (findUserIndex === -1) return response.sendStatus(404);
+  request.findUserIndex = findUserIndex;
+  next();
 };
 
 //app.use(loggingMiddleware); // user middleware globally, will be called for each function
 
 // base route
 app.get("/", loggingMiddleware, (request, response) => {
-    response.status(201).send({msg : "Hello World!"});
+  response.status(201).send({ msg: "Hello World!" });
 });
 
 // common practice
-app.get('/api/users', (request, response) => {
-    const { query: { filter, value } } = request;
+app.get(
+  "/api/users",
+  query("filter")
+    .isString()
+    .notEmpty()
+    .withMessage("Must not be emtpy")
+    .isLength({ min: 3, max: 10 })
+    .withMessage("Must be at least 3-10 characters"),
+  (request, response) => {
+    const result = validationResult(request);
+    console.log(result);
+    const {
+      query: { filter, value },
+    } = request;
     if (filter && value)
-        return response.status(201).send(mockUsers.filter((user) => user[filter].includes(value)));
+      return response
+        .status(201)
+        .send(mockUsers.filter((user) => user[filter].includes(value)));
     return response.status(201).send(mockUsers);
-});
+  }
+);
 
-app.post('/api/users', (request, response) => {
-    const { body } = request;
-    const newUser = {id: mockUsers[mockUsers.length - 1].id + 1, ...body};
+app.post(
+  "/api/users",
+  checkSchema(createUserValidationSchema),
+  (request, response) => {
+    const result = validationResult(request);
+    console.log(result);
+
+    if (!result.isEmpty())
+      return response.status(400).send({ errors: result.array() });
+
+    const data = matchedData(request);
+    const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...data };
     console.log(newUser);
     mockUsers.push(newUser);
     return response.status(200).send(newUser);
+  }
+);
+
+app.get("/api/products", (request, response) => {
+  response
+    .status(201)
+    .send({ products: [{ id: 1, product: "Lamp", price: 12.99 }] });
 });
 
-app.get('/api/products', (request, response) => {
-    response.status(201).send({products: [
-        {id: 1, product: "Lamp", price: 12.99}
-    ]})
+app.get("/api/users/:id", resolveIndexByUserId, (request, response) => {
+  const { findUserIndex } = request;
+  const findUser = mockUsers[findUserIndex];
+  if (!findUser) return response.sendStatus(404);
+  return response.send(findUser);
 });
 
-app.get('/api/users/:id', resolveIndexByUserId, (request, response) => {
-    const { findUserIndex } = request;
-    const findUser = mockUsers[findUserIndex];
-    if (!findUser) return response.sendStatus(404);
-    return response.send(findUser)
+app.put("/api/users/:id", resolveIndexByUserId, (request, response) => {
+  const { body, findUserIndex } = request;
+  mockUsers[findUserIndex] = { id: mockUsers[findUserIndex].id, ...body };
+  return response.sendStatus(200);
 });
 
-app.put('/api/users/:id', resolveIndexByUserId, (request, response) => {
-    const { body, findUserIndex } = request;
-    mockUsers[findUserIndex] = {id: mockUsers[findUserIndex].id, ...body};
-    return response.sendStatus(200);
+app.patch("/api/users/:id", resolveIndexByUserId, (request, response) => {
+  const { body, findUserIndex } = request;
+  mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body };
+  return response.sendStatus(200);
 });
 
-app.patch('/api/users/:id', resolveIndexByUserId, (request, response) => {
-    const { body, findUserIndex } = request;
-    mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body };
-    return response.sendStatus(200);
-});
-
-app.delete('/api/users/:id', resolveIndexByUserId, (request, response) => {
-    const { findUserIndex } = request;
-    mockUsers.splice(findUserIndex, 1);
-    return response.sendStatus(200);
+app.delete("/api/users/:id", resolveIndexByUserId, (request, response) => {
+  const { findUserIndex } = request;
+  mockUsers.splice(findUserIndex, 1);
+  return response.sendStatus(200);
 });
 
 app.listen(PORT, () => {
-    console.log(`Running on port ${PORT}`);
+  console.log(`Running on port ${PORT}`);
 });
-
 
 // create an express app, then call listen on the port we want our server to run on
 // request hold all information about the user who made the request
