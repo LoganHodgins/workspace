7a59da4cc1955529463bbe0c925c01d74b3a83b6
Logan <loganhodginsofficial@gmail.com>
2025-01-03 21:10:22 -0800

    CS-1111

--- express-tutorial/src/index.mjs
+++ express-tutorial/src/index.mjs
@@ -1,15 +1,18 @@
 import express from "express";
 import routes from "./routes/index.mjs";
+import cookieParser from "cookie-parser";
 import { loggingMiddleware } from "./utils/middlewares.mjs";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser("helloworld"));
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", loggingMiddleware, (request, response) => {
+  response.cookie("hello", "world", { maxAge: 30000, signed: true });
   response.status(201).send({ msg: "Hello World!" });
 });
 
--- express-tutorial/src/routes/products.mjs
+++ express-tutorial/src/routes/products.mjs
@@ -3,9 +3,12 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/api/products", (request, response) => {
-    response
-      .status(201)
-      .send({ products: [{ id: 1, product: "Lamp", price: 12.99 }] });
-  });
+    console.log(request.signedCookies);
+    if (request.signedCookies.hello && request.signedCookies.hello === "world")
+      return response.status(201).send({ products: [{ id: 1, product: "Lamp", price: 12.99 }] });
+
+    return response.status(403).send({ msg: "Sorry. You need the correct cookie."});
+  }
+);
 
 export default router;
