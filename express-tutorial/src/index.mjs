48823b0a1c84c36aacc2e239a309c5e8299564b3
Logan <loganhodginsofficial@gmail.com>
2025-01-05 00:19:33 -0800

    CS-1111 sessions

--- express-tutorial/src/index.mjs
+++ express-tutorial/src/index.mjs
@@ -1,17 +1,31 @@
 import express from "express";
 import routes from "./routes/index.mjs";
 import cookieParser from "cookie-parser";
+import session from "express-session";
 import { loggingMiddleware } from "./utils/middlewares.mjs";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser("helloworld"));
+app.use(
+  session({
+    secret: "test",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 60000 * 60,
+    },
+  })
+);
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", loggingMiddleware, (request, response) => {
+  console.log(request.session);
+  console.log(request.session.id);
+  request.session.visited = true;
   response.cookie("hello", "world", { maxAge: 30000, signed: true });
   response.status(201).send({ msg: "Hello World!" });
 });
