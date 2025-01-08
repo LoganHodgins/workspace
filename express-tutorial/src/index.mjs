45f2e77a66e6ceab3ccdfa4da3cca6da4acc5582
Logan <loganhodginsofficial@gmail.com>
2025-01-07 22:27:26 -0800

    CS-1111

--- express-tutorial/src/index.mjs
+++ express-tutorial/src/index.mjs
@@ -60,11 +60,8 @@ app.post("/api/cart", (request, response) => {
   if (!request.session.user) return response.sendStatus(401);
   const { body: item } = request;
   const { cart } = request.session;
-  if (cart) {
-    cart.push(item);
-  } else {
-    request.session.cart = [item];
-  }
+  if (cart) cart.push(item);
+  else request.session.cart = [item];
   return response.status(201).send(item);
 });
 
