6e01865b333ed37599126b2ae124855be9e89a74
Logan <loganhodginsofficial@gmail.com>
2025-01-11 22:21:36 -0800

    CS-1111

--- express-tutorial/src/index.mjs
+++ express-tutorial/src/index.mjs
@@ -1,11 +1,10 @@
-import express, { request } from "express";
+import express from "express";
 import routes from "./routes/index.mjs";
 import cookieParser from "cookie-parser";
 import session from "express-session";
 import passport from "passport";
 import "./strategies/local-strategy.mjs";
 import { loggingMiddleware } from "./utils/middlewares.mjs";
-import { mockUsers } from "./utils/constants.mjs";
 
 const app = express();
 
@@ -39,34 +38,4 @@ app.get("/", loggingMiddleware, (request, response) => {
   request.session.visited = true;
   response.cookie("hello", "world", { maxAge: 30000, signed: true });
   response.status(201).send({ msg: "Hello World!" });
-});
-
-app.post("/api/auth", passport.authenticate("local"), (request, response) => {
-  response.sendStatus(200);
-});
-
-app.get("/api/auth/status", (request, response) => {
-  return request.user ? response.send(request.user) : response.sendStatus(401);
-});
-
-app.post("/api/auth/logout", (request, response) => {
-  if (!request.user) return response.sendStatus(401);
-  request.logout((err) => {
-    if (err) return response.sendStatus(400);
-    response.sendStatus(200);
-  });
-});
-
-app.post("/api/cart", (request, response) => {
-  if (!request.session.user) return response.sendStatus(401);
-  const { body: item } = request;
-  const { cart } = request.session;
-  if (cart) cart.push(item);
-  else request.session.cart = [item];
-  return response.status(201).send(item);
-});
-
-app.get("/api/cart", (request, response) => {
-  if (!request.session.user) return response.sendStatus(401);
-  return response.send(request.session.cart ?? []);
 });
